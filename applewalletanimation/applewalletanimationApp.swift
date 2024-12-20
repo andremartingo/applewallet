ed33a0bb4d9946d9f50a0137294a17a6c271e9d6
Andre Martingo <andre.martingo@gmail.com>
2024-12-20 15:08:14 +0000

    Initial Commit

---
+++ applewalletanimation/applewalletanimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  applewalletanimationApp.swift
+//  applewalletanimation
+//
+//  Created by André Martingo on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct applewalletanimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
